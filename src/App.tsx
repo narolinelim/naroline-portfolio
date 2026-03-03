0041fd508d746c6e25cca9db3aaf996d29bd1029
narolinelim <narolinel@student.unimelb.edu.au>
2026-03-03 22:57:13 +1100

    duck

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
 
   return (
     <div>
-      <nav className="flex justify-between px-8 py-4 z-50 sticky top-0 bg-transparent">
-        <a href="#aboutme-section" className="text-xl font-bold">Naroline Lim</a>
-        <div className="flex space-x-10">
+      <nav className="flex justify-between px-10 py-4 z-50 sticky top-0 bg-transparent items-center">
+        <a href="#aboutme-section" className="text-3xl font-bold">Naroline Lim 😏</a>
+        <div className="flex space-x-10 border border-black rounded-md p-5">
           <a href="#aboutme-section">About Me</a>
           <a href="#project-section">Projects</a>
           <a href="#resume-section">Resume</a>
--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -1,4 +1,5 @@
 import { ScrollContainer, Animator, ScrollPage, batch, Fade, FadeOut, FadeIn, MoveOut, Move, ZoomOut, ZoomIn } from 'react-scroll-motion';
+import patoDuck from "../assets/pato-duck.gif";
 
 
 const FadeUp = batch(FadeIn(), ZoomOut());
@@ -10,9 +11,14 @@ function AboutMe() {
                 <ScrollPage>
                     <div className="flex justify-center items-center h-screen">
                         <Animator animation={FadeUp}>
-                            <div>
-                                <Animator animation={Fade()}>
+                            <div className="flex flex-row items-center space-x-20">
+                                <Animator animation={batch(Fade(), MoveOut(-1000, 0))}>
                                     <h1>Naroline Lim</h1>
+                                    <p>In Pursuit Of Greatness.</p>
+                                </Animator>
+                                
+                                <Animator animation={MoveOut(1000, 0)}>
+                                    <img src={patoDuck} alt="description" />
                                 </Animator>
                             </div>
                         </Animator>
