3997ce19e98eab1b71d3b07a7243f797c2870efe
narolinelim <narolinel@student.unimelb.edu.au>
2026-03-24 14:43:34 +1100

    fix: changed the timeline library

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,57 +1,56 @@
 import { ScrollContainer, Animator, ScrollPage, batch, Fade, FadeOut, FadeIn, MoveOut, Move } from 'react-scroll-motion';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
 
 const FadeUp = batch(Fade(), Move());
 
 function Resume() {
-    return (
-        <div id="resume-section">
-            <ScrollContainer>
-                <ScrollPage>
-                    <div className="flex justify-center items-center h-screen">
-                        <Animator animation={FadeUp}>
-                            <div className="w-200">
-                              <Animator animation={Fade()}>
-                                  <VerticalTimeline>
-                                    <VerticalTimelineElement
-                                      className="vertical-timeline-element--work"
-                                      contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                                      contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                                      date="November 2025 - March 2026"
-                                    >
-                                      <h3 className="vertical-timeline-element-title">To be continued...</h3>
-                                      <h4 className="vertical-timeline-element-subtitle">Earth</h4>
-                                    </VerticalTimelineElement>
-                                    <VerticalTimelineElement
-                                      className="vertical-timeline-element--work"
-                                      contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                                      contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                                      date="November 2025 - March 2026"
-                                    >
-                                      <h3 className="vertical-timeline-element-title">Research Software Engineering Intern</h3>
-                                      <h4 className="vertical-timeline-element-subtitle">Melbourne, VIC</h4>
-                                    </VerticalTimelineElement>
-                                    <VerticalTimelineElement
-                                      className="vertical-timeline-element--work"
-                                      contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                                      contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                                      date="November 2025 - March 2026"
-                                    >
-                                      <h3 className="vertical-timeline-element-title">Fetus core</h3>
-                                      <h4 className="vertical-timeline-element-subtitle">Melbourne, VIC</h4>
-                                    </VerticalTimelineElement>
-                                  </VerticalTimeline>
-                              </Animator>
-                            </div>
-                        </Animator>
-                    </div>
-                </ScrollPage>
+  return (
+    <div id="resume-section">
+      <ScrollContainer>
+        <ScrollPage>
+          <div className="flex justify-center items-center h-screen">
+            <Animator animation={FadeUp}>
+              <div className="w-300">
+                <Animator animation={Fade()}>
+                  <Timeline position="alternate">
+                    <TimelineItem>
+                      <TimelineSeparator>
+                        <TimelineConnector sx={{ bgcolor: 'blue'}} />
+                        <TimelineDot>
+                          <RepeatIcon />
+                        </TimelineDot>
+                        <TimelineConnector />
+                      </TimelineSeparator>
+                      <TimelineContent sx={{ py: '12px', px: 2 }}>
+                        <Typography variant="h5" component="span">
+                          Research Software Engineer Intern
+                        </Typography>
+                        <Typography variant="h6">WEHI</Typography>
+                        <Typography>Built a budget forecasting web app with Shiny</Typography>
+                      </TimelineContent>
+                    </TimelineItem>
+                  </Timeline>
+                </Animator>
+              </div>
+            </Animator>
+          </div>
+        </ScrollPage>
 
-            </ScrollContainer>
-        </div>
-    
-    );
+      </ScrollContainer>
+    </div>
+  
+  );
 }
 
 
